4ad59d5ed01dfb09a2b58434fff6cd0b7dd84124
DarTea770 <mark2love2@yandex.ru>
2022-03-14 20:58:43 +0300

    slon_game

---
+++ alice.py
@@ -0,0 +1,66 @@
+from flask import Flask, request
+import logging
+import json
+
+app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+
+sessionStorage = {}
+
+
+@app.route('/post', methods=['POST'])
+def main():
+    logging.info(f'Request: {request.json!r}')
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+    handle_dialog(request.json, response)
+    logging.info(f'Response:  {response!r}')
+    return json.dumps(response)
+
+
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+
+    if req['session']['new']:
+        sessionStorage[user_id] = {
+            'suggests': ["Не хочу.", "Не буду.", "Отстань!",]
+        }
+        res['response']['text'] = 'Привет! Купи слона!'
+        res['response']['buttons'] = get_suggests(user_id)
+        return
+
+    if req['request']['original_utterance'].lower() in ['ладно',
+                                                        'куплю', 'уговорила', 'хорошо']:
+        res['response']['text'] = 'Отлично! Слона можно найти на Яндекс.Маркете!'
+        res['response']['end_session'] = True
+        return
+
+    res['response']['text'] = \
+        f"Все говорят '{req['request']['original_utterance']}'( А ты порадуй меня - купи слона!"
+    res['response']['buttons'] = get_suggests(user_id)
+
+
+def get_suggests(user_id):
+    session = sessionStorage[user_id]
+    suggests = [
+        {'title': suggest, 'hide': True}
+        for suggest in session['suggests'][:2]
+    ]
+    session['suggests'] = session['suggests'][1:]
+    sessionStorage[user_id] = session
+    if len(suggests) < 2:
+        suggests.append({
+            "title": "Ладно",
+            "url": "https://market.yandex.ru/search?text=слон",
+            "hide": True
+        })
+    return suggests
+
+
+if __name__ == '__main__':
+    app.run()
